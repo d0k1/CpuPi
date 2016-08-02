9b8fc26f4f3d87f4f10300342b797f6b20502612
Denis Kirpichenkov <denis.kirpichenkov@gmail.com>
2016-08-02 19:39:21 +0500

    Update pi.groovy

--- pi.groovy
+++ pi.groovy
@@ -1,5 +1,7 @@
-// the more complexity the more cpu gets burned
-int complexity = 2000;
+// the more complexity the more cpu to burn. Default value is huge
+int complexity = 200000;
+// how  many threads use to burn. Default value - burn just one core (cpu thread)
+int threadCount = 1;
 
 public class PI
 {
@@ -30,14 +32,32 @@ public class PI
             pi.append(String.format("%04d", (long)(carry + (sum / SCALE))));
             carry = sum % SCALE;
         }
-        return pi.toString();
+        return pi.toString();Очистить
+threadCount
     }
 }
 
+def burn(def complexity)
+{
 long millis0 = System.currentTimeMillis();
 System.out.println("Start");
 int n = complexity; // 200000;
 String result = new PI().pi_digits(n);
 long millis1 = System.currentTimeMillis();
 logger.info("Done. Time spent: "+(millis1-millis0)+" ms");
-logger.info("!PI is equal "+result);
+logger.info("!!! Threads: ${Thread.currentThread().getName()} PI is equal "+result);
+}
+
+def threads = [];
+
+for(int i=0;i<threadCount;i++){
+  threads << Thread.start{
+    burn(complexity);
+  };
+}
+
+threads.each({it->
+  it.join();
+})
+
+return null;
